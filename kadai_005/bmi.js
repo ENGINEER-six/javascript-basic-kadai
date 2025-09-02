e046e1b681e97e06dcaabd25ce4a03690a9f3a76
ENGINEER-six <out.six.look@outlook.jp>
2025-09-02 10:36:42 +0900

    kadai_005追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,23 @@
+// 体重
+// 変数の宣言
+let bodyWeight;
+
+// 値の代入
+bodyWeight = 68;
+
+// 身長
+// 変数の宣言
+let height;
+
+// 値の代入
+height = 1.7;
+
+// BMI
+// 変数の宣言
+let bmi;
+
+// 値の代入
+bmi = (bodyWeight)/(height*height);
+
+// コンソールへの出力
+console.log(bmi);
