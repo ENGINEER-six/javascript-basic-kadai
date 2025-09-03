160debb590bd1771dfce6e02871bc2db5fbe506b
ENGINEER-six <out.six.look@outlook.jp>
2025-09-03 11:33:10 +0900

    kadai_mdn_js修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
 // 現在の年月日や時刻の取得
-const today = new Date("2025-09-03");
+const today = new Date();
 
 // 取得した年のみ
-const year = today.getFullYear();
+const year = today.getUTCFullYear();
 
 // 取得した月のみ
 const month = today.getMonth() + 1;
@@ -10,4 +10,5 @@ const month = today.getMonth() + 1;
 // 取得した日のみ
 const day = today.getDate();
 
-console.log(year + '年',month + '月',day + '日');
+// 取得した年月日の出力
+console.log(`${year}年${month}月${day}日`);
